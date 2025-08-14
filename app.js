ac32863588864762d392f7f86c49bf511bf00283
AlessandroMassulo <alessandro.massulo@gmail.com>
2025-08-14 00:37:44 -0300

    Inseridas as funcoes iniciais

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function textFromField(idCampo){
+      let inputElement = document.getElementById(idCampo);
+      let textFromField = inputElement.value;
+     return textFromField;
+}
+
+function cleanField(idCampo){
+    let inputElement =document.getElementById(idCampo);
+    inputElement.value ="";
+}
+
+function changeText(idCampo,newText){
+    let inputElement = document.getElementById(idCampo);
+    inputElement.value=newText;
+}
+
+listaAmigos
+
+function adicionarAmigo() {
+    let novoAmigo = textFromField('amigo');
+    if (novoAmigo === "") {
+        alert('Por favor, insira um nome.');
+    } else if (amigos.includes(novoAmigo)) {
+        alert('O nome já consta na lista, favor alterar')
+    } else {
+        amigos.push(novoAmigo);
+    }
+    changeText('listaAmigos',amigos)        
+    cleanField('amigo');       
+console.log(amigos)
+}
